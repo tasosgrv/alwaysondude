599fb391c514ef7abc2984ce947e2ae3c8d64230
tasosgrv <tasosgr4@gmail.com>
2021-01-11 21:24:58 +0200

    fixed bot losing more point when user wins

--- cogs/games.py
+++ cogs/games.py
@@ -60,7 +60,7 @@ class Games(commands.Cog):
             
             if games.Coinflip(self.bet, user, reaction).result:
 
-                self.db.setPoints(channel.guild.name, self.client.user.id, banker_points-(self.bet*2))
+                self.db.setPoints(channel.guild.name, self.client.user.id, banker_points-self.bet)
                 self.db.setPoints(channel.guild.name, user.id, player_points+self.bet)
                 embed = discord.Embed(title = f"[BETA]:coin: : :white_check_mark: {user.name} **WON** with {reaction.emoji}",
                                         color= discord.Color.green(),
