57185632a2945b028c2da33547fd8b38c26ad0d4
tasosgrv <tasosgr4@gmail.com>
2021-08-03 14:14:06 +0300

    ftk aliasses daily reward

--- cogs/myevents.py
+++ cogs/myevents.py
@@ -4,7 +4,7 @@ import math
 import database
 import pprint as debug
 from datetime import datetime
-from discord.ext import commands
+from discord.ext import tasks, commands
 
 
 
@@ -14,22 +14,23 @@ class MyEvents(commands.Cog):
     def __init__(self, client):
         self.client = client
         self.db = database.Database()
+        self.chpresence.start()
 
 
     @commands.Cog.listener()  
     async def on_ready(self):
         print(f'We have logged in as {self.client.user}')  # notification of login.
 
-        await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name='my master to learn new things'))
-    
+        await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f'your bets 💰 in {len(self.client.guilds)} servers'))
+        
         self.db.connect()
         for guild in self.client.guilds:
             self.db.insert('guilds', guild.id, guild.name, guild.chunked, guild.member_count, guild.owner_id)
             self.db.createTable(guild.name)
             for member in guild.members:
-                if member.name == self.client.user.name:
-                   self.db.insert(member.guild.name , member.id, member.name, member.discriminator, member.bot, member.nick, True, 5000, member.guild.id) 
-                self.db.insert(member.guild.name , member.id, member.name, member.discriminator, member.bot, member.nick, True, 0, member.guild.id)
+                if member.id == self.client.user.id:
+                   self.db.insert(member.guild.name , member.id, member.name, member.discriminator, member.bot, member.nick, True, 5000, member.guild.id, "2000-01-01 00:00:00.00") 
+                self.db.insert(member.guild.name , member.id, member.name, member.discriminator, member.bot, member.nick, True, 0, member.guild.id, "2000-01-01 00:00:00.00")
         self.db.close_connection()
         print(f'Database update complete')   
 
@@ -38,7 +39,7 @@ class MyEvents(commands.Cog):
     @commands.Cog.listener()
     async def on_member_join(self, member):
         self.db.connect()
-        self.db.insert(member.guild.name, member.id, member.name, member.discriminator, member.bot, member.nick, True, 0, member.guild.id)
+        self.db.insert(member.guild.name, member.id, member.name, member.discriminator, member.bot, member.nick, True, 0, member.guild.id, "2000-01-01 00:00:00.00")
         self.db.close_connection()
 
     @commands.Cog.listener()
@@ -58,42 +59,54 @@ class MyEvents(commands.Cog):
         self.db.insert('guilds', guild.id, guild.name, guild.chunked, guild.member_count, guild.owner_id)
         self.db.createTable(guild.name)
         for member in guild.members:
-            if member.name == self.client.name:
-                self.db.insert(member.guild.name , member.id, member.name, member.discriminator, member.bot, member.nick, True, 5000, member.guild.id) 
-            self.db.insert(guild.name, member.id, member.name, member.discriminator, member.bot, member.nick, True, 10, member.guild.id)
+            if member.id == self.client.user.id:
+                self.db.insert(member.guild.name, member.id, member.name, member.discriminator, member.bot, member.nick, True, 5000, member.guild.id, "2000-01-01 00:00:00") 
+            self.db.insert(guild.name, member.id, member.name, member.discriminator, member.bot, member.nick, True, 10, member.guild.id, "2000-01-01 00:00:00")
         self.db.close_connection()
+        print(f"Joined in {guild.name}")
+        logging.info(f"Joined in {guild.name}")
 
     @commands.Cog.listener()
     async def on_guild_remove(self, guild):
         '''
         When the bot leaves a server\n
-        The guilded deleted from the database
+        The guild deleted from the database
         '''
         self.db.connect()
         self.db.delete('guilds', guild.id)
+        self.db.dropTable(guild.name)
         self.db.close_connection()
+        print(f"{guild.name} removed me ")
+        logging.info(f"{guild.name} removed me ")
+
 
 
     @commands.Cog.listener()
     async def on_message(self, message):  # event that happens per any message.
         # each message has a bunch of attributes. Here are a few.
         # check out more by print(dir(message)) for example.
+
+        '''
         logging.info(f"{message.guild.name}: {message.channel}: {message.author}: {message.author.name}: {message.content}")
         print(f"{message.channel}: {message.author}: {message.author.name}: {message.content}")
-        if message.author.bot :
+        '''
+
+        if message.author.bot:
             return
         
         if not message.content.startswith('.') and len(message.content)<211:
             self.db.connect()
             points = self.db.getPoints(message.guild.name, message.author.id)
-            gained_points = math.floor(len(message.content)*0.12)
+            gained_points = len(message.content)*0.12
             points += gained_points
-            self.db.setPoints(message.guild.name, message.author.id, points)
-            self.db.close_connection()
-            if gained_points>0:
+
+            if gained_points>1:
+                self.db.setPoints(message.guild.name, message.author.id, points)
+            
                 print(f"{message.guild.name}: {message.author} gained {gained_points} points")
                 logging.info(f"{message.guild.name}: {message.author} gained {gained_points} points")
-
+            
+            self.db.close_connection()
 
         if "hello" in message.content.lower():
             await message.channel.send(f"Hello {message.author.mention}, i am your digital friend {self.client.user.mention}, Do not panic i am peaceful. Mr. w0ch4 is my master!")
@@ -101,9 +114,10 @@ class MyEvents(commands.Cog):
         if  "fuck" in message.content.lower():
             await message.channel.send(f"Go to hell {message.author.mention}")
 
+
     @commands.Cog.listener() 
     async def on_reaction_add(self, reaction, user):
-        if user.bot  or reaction.message.author==self.client.user: #if the reaction made from a bot or if the reaction made to the client
+        if user.bot or reaction.message.author==self.client.user: #if the reaction made from a bot or if the reaction made to the client
             return
         self.db.connect()
         points = self.db.getPoints(user.guild.name, user.id)
@@ -124,6 +138,18 @@ class MyEvents(commands.Cog):
         self.db.close_connection()
         print(f"{user.guild.name}: {user} lost {remove_points} points")
         logging.info(f"{user.guild.name}: {user} lost {remove_points} points")
+
+    
+    
+    @tasks.loop(seconds=1800.0)
+    async def chpresence(self):
+        await self.client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name=f'your bets 💰 in {len(self.client.guilds)} servers'))
+
+    @chpresence.before_loop
+    async def before_chpresence(self):
+        await self.client.wait_until_ready()
+
+
 
     '''
     @commands.Cog.listener()
