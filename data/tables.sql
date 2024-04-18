f0d2e20f8a2481c0a2eeb6462d8741dd1e059ea7
tasosgrv <tasosgr4@gmail.com>
2024-04-18 20:18:16 +0300

    tables

--- data/tables.sql
+++ data/tables.sql
@@ -1,4 +1,16 @@
-
+CREATE TABLE guilds(
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    chunked BOOLEAN NOT NULL,
+    member_count INT NOT NULL,
+    owner_id BIGINT NOT NULL,
+    prefix CHAR(1) NOT NULL DEFAULT '.',
+    earning_rate REAL NOT NULL DEFAULT 0.12,
+    drops BOOLEAN NOT NULL DEFAULT TRUE,
+    daily_reward REAL NOT NULL DEFAULT 25,
+    currency_name VARCHAR(15) NOT NULL DEFAULT 'coins',
+    currency_emote VARCHAR(54) NOT NULL DEFAULT ':moneybag:',
+)
 
 
 
@@ -13,4 +25,4 @@ CREATE TABLE IF NOT EXISTS {table} (
     guild_id BIGINT REFERENCES guilds(id) 
     ON DELETE CASCADE,
     dailyreward timestamp
-)
+);
